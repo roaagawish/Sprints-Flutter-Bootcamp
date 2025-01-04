2a5ec25f5953d0640f624461d35adac4a3007d62
roaa gawish <roaa.ayman.mail@gmail.com>
2025-01-04 22:46:22 +0200

    upload signUp page task

---
+++ Flutter Mobile Application Development/Flutter Fundamentals/Input Validation - Navigation/sign_up_page/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:sign_up_page/sign_up.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Color(0xffd0aad1)),
+        useMaterial3: true,
+      ),
+      home: SignUp(),
+    );
+  }
+}
